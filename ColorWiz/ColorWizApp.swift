902f763459278ce426904c02a4e3f7e39e364d75
Aidan Kang <akang@lcs.on.ca>
2023-06-01 14:25:03 -0400

    Initial Commit

---
+++ ColorWiz/ColorWizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorWizApp.swift
+//  ColorWiz
+//
+//  Created by Aidan Kang on 2023-06-01.
+//
+
+import SwiftUI
+
+@main
+struct ColorWizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
